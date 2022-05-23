aaf81f43a3edeabf29780981bc72e950e02956c7
KIBA <kjh030529@gmail.com>
2022-05-23 23:55:58 +0900

    Change Chapter 13 - Question 13 script & Excutable program file

--- 과제8/14_8/main.c
+++ 과제8/14_8/main.c
@@ -3,18 +3,17 @@
 
 
 int main() {
-    int arr[8][8] ={};
-
-	int (*harr)[8] = arr;
+    int** arr = (int**)malloc(sizeof(int*)*8);// Last in \0
 	for(int i = 0; i< 8; i++)
 	{
+		arr[i] = (int*)malloc(sizeof(int)*8);
 		for(int j = 0; j< 8; j++)
 		{
-			(*(harr+i))[j] = i * 10 + j;
+			*(arr[i]+j) = i * 10 + j;
 		}
 	}
 
-	printf("%d", arr[3][4]);
+    printf("%d", arr[3][4]);
 	int i=0;
 	scanf("%d", &i);
 }
